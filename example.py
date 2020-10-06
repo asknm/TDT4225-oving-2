544971bf8800184527271764ab76611f23c45321
sivert <sivertb96@gmail.com>
2020-10-06 14:08:48 +0200

    insert methods

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 from DbConnector import DbConnector
 from tabulate import tabulate
+from testing import load
 
 
 class ExampleProgram:
@@ -9,22 +10,58 @@ class ExampleProgram:
         self.db_connection = self.connection.db_connection
         self.cursor = self.connection.cursor
 
-    def create_table(self, table_name):
+    def create_user_table(self, table_name):
         query = """CREATE TABLE IF NOT EXISTS %s (
-                   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-                   name VARCHAR(30))
+                id VARCHAR(50) NOT NULL PRIMARY KEY,
+                has_labels BOOLEAN)
                 """
         # This adds table_name to the %s variable and executes the query
         self.cursor.execute(query % table_name)
         self.db_connection.commit()
 
-    def insert_data(self, table_name):
-        names = ['Bobby', 'Mc', 'McSmack', 'Board']
-        for name in names:
-            # Take note that the name is wrapped in '' --> '%s' because it is a string,
-            # while an int would be %s etc
-            query = "INSERT INTO %s (name) VALUES ('%s')"
-            self.cursor.execute(query % (table_name, name))
+    def create_activity_table(self, table_name):
+        query = """CREATE TABLE IF NOT EXISTS %s (
+                id INT NOT NULL PRIMARY KEY,
+                user_id VARCHAR(50) NOT NULL references user(id),
+                transportation_mode VARCHAR(20),
+                start_date_time DATETIME,
+                end_date_time DATETIME)
+                """
+        # This adds table_name to the %s variable and executes the query
+        self.cursor.execute(query % table_name)
+        self.db_connection.commit()
+
+    def create_trackpoint_table(self, table_name):
+        query = """CREATE TABLE IF NOT EXISTS %s (
+                id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                activity_id INT NOT NULL references activity(id),
+                lat DOUBLE,
+                lon DOUBLE,
+                altitude INT,
+                date_days DOUBLE,
+                date_time DATETIME)
+                """
+        # This adds table_name to the %s variable and executes the query
+        self.cursor.execute(query % table_name)
+        self.db_connection.commit()
+
+    def insert_user_data(self, data):
+        query = "INSERT INTO user VALUES (%(id)s, %(has_labels)s)"
+        self.cursor.executemany(query, data)
+        self.db_connection.commit()
+
+    def insert_activity_data(self, data):
+        query = """INSERT INTO user VALUES (%(id)s, %(user_id)s, %(transportation_mode)s, 
+                %(start_date_time)s, %(end_date_time)s)
+                """
+        self.cursor.executemany(query, data)
+        self.db_connection.commit()
+
+    def insert_trackpoint_data(self, data):
+        query = """INSERT INTO trackpoint VALUES (%(id)s, %(activity_id)s, %(lat)s, %(lon)s
+                %(altitude)s, %(date_days)s, %(date_time)s)
+        """
+        self.cursor.executemany(query, data)
         self.db_connection.commit()
 
     def fetch_data(self, table_name):
@@ -42,27 +79,25 @@ class ExampleProgram:
         print("Dropping table %s..." % table_name)
         query = "DROP TABLE %s"
         self.cursor.execute(query % table_name)
-
-    def show_tables(self):
+        query = """ALTER TABLE %s
+                        ADD """
         self.cursor.execute("SHOW TABLES")
         rows = self.cursor.fetchall()
-        print(tabulate(rows, headers=self.cursor.column_names))
 
 
 def main():
     program = None
     try:
         program = ExampleProgram()
-        program.create_table(table_name="User")
-        program.create_table(table_name="Activity")
-        program.create_table(table_name="TrackPoint")
-        # program.insert_data(table_name="Person")
-        # _ = program.fetch_data(table_name="Person")
-        program.drop_table(table_name="User")
-        program.drop_table(table_name="Activity")
-        program.drop_table(table_name="TrackPoint")
-        # Check that the table is dropped
-        program.show_tables()
+        program.create_user_table(table_name="user")
+        program.create_activity_table(table_name="activity")
+        program.create_trackpoint_table(table_name="trackpoint")
+        data = load("format_test.txt")
+        program.insert_user_data(data)
+        _ = program.fetch_data(table_name="user")
+        # program.drop_table(table_name="user")
+        # program.drop_table(table_name="activity")
+        # program.drop_table(table_name="trackpoint")
     except Exception as e:
         print("ERROR: Failed to use database:", e)
     finally:
